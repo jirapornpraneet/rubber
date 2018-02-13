ec8f802e6193a33a147bb2efafdcfe992bb935ba
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-13 14:24:46 +0700

    Initial commit

---
+++ tire/tire/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 13/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
