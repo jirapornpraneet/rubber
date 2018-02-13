26809ff037cd23986c29ded1391d2c0e766122bd
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-13 21:27:50 +0700

    เพิ่มคลาส BaseManager

---
+++ tire/tire/NetworkManager/BaseManager.swift
@@ -0,0 +1,13 @@
+//
+//  BaseManager.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 13/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import UIKit
+
+class BaseManager: NSObject {
+
+}
