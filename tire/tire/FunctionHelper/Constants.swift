c870fb094ea91ea9f1caed97fa77f09371259dbb
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-15 21:32:38 +0700

    เพิ่มคลาส Constants

---
+++ tire/tire/FunctionHelper/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 15/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import UIKit
+
+class Constants: NSObject {
+    static let apiUrl = "https://time-api.biovii.com"
+    static let thumborSecureKey = "YOUR_SECURE_KEY"
+    static let thumborUrl = "https://cloud.skylpr.com/thumbor"
+    static let forgotPasswordUrl = "https://cloud.skylpr.com/index.php/user/forgot-password"
+}
