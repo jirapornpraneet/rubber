66a9c1f16977f45467742736c4a4346db9759a0e
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-16 13:14:46 +0700

    เพิ่มคลาส CustomerResource

---
+++ tire/tire/Resource/CustomerResource.swift
@@ -0,0 +1,20 @@
+//
+//  CustomerResource.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 16/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import Foundation
+import EVReflection
+
+public class CustomerResource: EVObject {
+    var firstName: String = ""
+    var lastName: String = ""
+    var carBrand: String = ""
+    var licensePlate: String = ""
+    var province: String = ""
+    var address: String = ""
+    var email: String = ""
+}
