fde380bc19e13939fc10ba2f962cc4da9a76cc43
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-16 13:22:41 +0700

    เพิ่มคลาส LoginResource

---
+++ tire/tire/Resource/LoginResource.swift
@@ -0,0 +1,14 @@
+//
+//  LoginResource.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 16/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import UIKit
+import EVReflection
+
+class LoginResource: EVObject {
+    var accessToken: String?
+}
