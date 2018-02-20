721ff28012a174cb741c01132f4166cd1b80d5a8
Jiraporn Praneet <jirapornpraneet@Jiraporns-MacBook-Pro.local>
2018-02-20 21:56:23 +0700

    เพิ่มคลาส MainViewController และ NewCustomerViewController

---
+++ tire/tire/ViewController/NewCustomerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewCustomerViewController.swift
+//  tire
+//
+//  Created by Jiraporn Praneet on 20/2/2561 BE.
+//  Copyright © 2561 Jiraporn Praneet. All rights reserved.
+//
+
+import UIKit
+
+class NewCustomerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
